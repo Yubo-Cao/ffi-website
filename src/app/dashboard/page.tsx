360e2ecfe17ff71e11814b636ed16f97f209f0b8
Yubo-Cao <cao2006721@gmail.com>
2024-10-07 19:57:56 -0400

    fix: remove cookies in dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,7 @@
 import Breadcrumb from "@/components/Common/Breadcrumb";
 import EnrolledCourses from "@/components/Dashboard/EnrolledCourses";
-import { clientConfig, serverConfig } from "@/lib/config";
 import { NAME } from "@/lib/constants";
 import { Metadata } from "next";
-import { getTokens } from "next-firebase-auth-edge";
-import { cookies } from "next/headers";
-import { notFound } from "next/navigation";
 
 export const metadata: Metadata = {
   title: `Dashboard | ${NAME}`,
@@ -13,15 +9,6 @@ export const metadata: Metadata = {
 };
 
 const DashboardPage = async () => {
-  const tokens = await getTokens(cookies(), {
-    apiKey: clientConfig.apiKey,
-    cookieName: serverConfig.cookieName,
-    cookieSignatureKeys: serverConfig.cookieSignatureKeys,
-    serviceAccount: serverConfig.serviceAccount,
-  });
-  if (!tokens) {
-    return notFound();
-  }
   return (
     <>
       <Breadcrumb
