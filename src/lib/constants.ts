8a45c7adc1af4675f23f9179e3abbb0ab6c232dd
Yubo-Cao <cao2006721@gmail.com>
2024-11-26 12:31:40 -0500

    feat: :sparkles: Change the font to something more readable

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,11 +1,11 @@
-import { Cormorant_Garamond, Inter } from "next/font/google";
+import { Noto_Serif, Inter } from "next/font/google";
 
 export const NAME = "Future Finance Inc.";
 export const DESCRIPTION =
   "Organization providing free, accessible financial education to all.";
 export const LOGO = "/images/logo.svg";
 
-export const FONT = Cormorant_Garamond({
+export const FONT = Noto_Serif({
   weight: ["400", "700"],
   subsets: ["latin"],
 });
